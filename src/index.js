4e02c761575d5bfa4272e97fbe250f081b7db97b
Pavlo Trykulich <trikulich00@gmail.com>
2023-08-09 22:55:58 +0300

    feat: implement code for check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const openedBrackets = [];
+  const closedBracketToOpened = {};
+
+  for (const [open, close] of bracketsConfig) {
+    closedBracketToOpened[close] = open;
+    openedBrackets.push(open);
+  }
+
+  const stack = [];
+
+  for (const char of str) {
+    if (openedBrackets.includes(char)) {
+      // Check if the opening bracket is the same as the closing bracket
+      // For cases with |, 1, 2  etc.
+      // char === closedBracketToOpened[char]
+      const lastInStack = stack[stack.length - 1];
+      if (char === lastInStack && char === closedBracketToOpened[char]) {
+        stack.pop();
+      } else {
+        stack.push(char);
+      }
+    } else {
+      const lastBracket = stack.pop();
+      if (lastBracket !== closedBracketToOpened[char]) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
 }
